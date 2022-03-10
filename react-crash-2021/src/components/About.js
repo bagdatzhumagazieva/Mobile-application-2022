c4e8c15087f074a50fed87ea8b6d946fed714a6e
bagdat <bagdon.zhumaagazieva@gmail.com>
2022-03-10 12:44:38 +0500

    first commit

---
+++ react-crash-2021/src/components/About.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+        <h4>Full name: Bagdat Zhumagaziyeva</h4>
+        <hr/>
+        <h4>ID: 21MD0150</h4>
+        <hr/>
+        <h4>Major: Software Engineering</h4>
+        <hr/>
+        <Link to='/'>Go Back</Link>
+    </div>
+  )
+}
+
+export default About
---
+++ react-crash-2021/src/components/Footer.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Link to='/about'>About me</Link>
+    </footer>
+  )
+}
+
+export default Footer
